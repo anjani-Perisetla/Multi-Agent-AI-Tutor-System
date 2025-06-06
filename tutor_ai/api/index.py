11214cca0440c19d1943bd96ac930da52f813dc1
anjani-Perisetla <anjanideviperisetla@gmail.com>
2025-06-05 20:06:58 -0700

    vercel deploy message comits

--- tutor_ai/api/index.py
+++ tutor_ai/api/index.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# Mount static files (CSS, JS)
+app.mount("/static", StaticFiles(directory="api/static"), name="static")
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI is live on Vercel!"}
+# Set up templates
+templates = Jinja2Templates(directory="api/templates")
+
+@app.get("/", response_class=HTMLResponse)
+def get_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
